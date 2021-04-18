241fcb50eb58b1d207091ee1534b928f327ceb5e
hprinsloo-godaddy <hprinsloo@godaddy.com>
2021-04-18 10:13:40 +0300

    Added Base project

---
+++ Shared/MinesweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinesweeperApp.swift
+//  Shared
+//
+//  Created by Hugo Prinsloo on 2021/04/18.
+//
+
+import SwiftUI
+
+@main
+struct MinesweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
